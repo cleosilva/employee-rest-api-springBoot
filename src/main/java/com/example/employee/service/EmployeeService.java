17befa8943aa3a66b4042491da0012c5c4e5baef
CLEOSILVA <cleo_silva1906@hotmail.com>
2023-04-12 18:27:53 -0300

    Create model, repository, service and database connection.

---
+++ src/main/java/com/example/employee/service/EmployeeService.java
@@ -0,0 +1,36 @@
+package com.example.employee.service;
+
+import com.example.employee.model.Employee;
+import com.example.employee.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+    @Autowired
+    EmployeeRepository empRepository;
+
+    public Employee createEmployee(Employee emp){
+        return empRepository.save(emp);
+    }
+
+    public List<Employee> getEmployess(){
+        return empRepository.findAll();
+    }
+
+    public void deleteEmployee(Long empId){
+        empRepository.deleteById(empId);
+    }
+
+    public Employee updateEmployee(Long empId, Employee employeeDetails){
+        Employee emp = empRepository.findById(empId).get();
+        emp.setFirstName(employeeDetails.getFirstName());
+        emp.setLastName(employeeDetails.getLastName());
+        emp.setEmailId(employeeDetails.getEmailId());
+
+        return empRepository.save(emp);
+    }
+
+}
